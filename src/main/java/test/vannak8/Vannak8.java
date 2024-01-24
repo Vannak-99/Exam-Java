a2304f2f29421325685e1ab77890c849eee5c8d4
LENOVO <LENOVO@Vannak>
2024-01-24 09:54:09 +0700

    First Commit

---
+++ src/main/java/test/vannak8/Vannak8.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.vannak8;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Vannak8 {
+
+    
+    
+}
